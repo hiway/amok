a2917f8c268681b41b5fe6c1553c4d6d06f45e1d
Harshad Sharma <harshad@sharma.io>
2023-06-27 16:11:15 +0530

    Add sign() and verify() to API

--- src/amok/api.py
+++ src/amok/api.py
@@ -1,16 +1,40 @@
+from base64 import b64encode, b64decode
+from secrets import token_bytes
 from typing import Optional
 
-from nacl.signing import SigningKey, VerifyKey
+import keyring
+from nacl.signing import SigningKey, VerifyKey, SignedMessage
+from nacl.encoding import HexEncoder
 
 
 class AmokAPI:
-    signing_key: Optional[SigningKey]
-    verify_key: Optional[VerifyKey]
-
     def __init__(self) -> None:
-        self.signing_key = None
-        self.verify_key = None
+        self._signing_key: Optional[SigningKey] = None
+        self._verify_key: Optional[VerifyKey] = None
+
+    @property
+    def verify_key(self) -> bytes:
+        assert self._verify_key is not None
+        return self._verify_key.encode(HexEncoder)
 
     async def init(self, name: str) -> None:
-        self.signing_key = SigningKey.generate()
-        self.verify_key = self.signing_key.verify_key
+        seed = keyring.get_password("amok", name)
+        if seed is None:
+            seed = token_bytes(32)
+            keyring.set_password("amok", name, b64encode(seed).decode("utf-8"))
+        else:
+            seed = b64decode(seed)
+        self._signing_key = SigningKey(seed)
+        self._verify_key = self._signing_key.verify_key
+
+    async def sign(self, message: bytes) -> SignedMessage:
+        assert self._signing_key is not None
+        return self._signing_key.sign(message)
+
+    async def verify(self, verify_key: bytes, message: bytes, signature: bytes) -> bool:
+        try:
+            _verify_key = VerifyKey(verify_key, HexEncoder)
+            _verify_key.verify(message, signature)
+            return True
+        except Exception:
+            return False
--- tests/test_api.py
+++ tests/test_api.py
@@ -6,10 +6,52 @@ from amok import AmokAPI
 async def test_init():
     amok = AmokAPI()
 
-    assert amok.signing_key is None
-    assert amok.verify_key is None
+    assert amok._signing_key is None
+    assert amok._verify_key is None
 
     await amok.init(name="Example")
 
-    assert isinstance(amok.signing_key, SigningKey)
-    assert isinstance(amok.verify_key, VerifyKey)
+    assert isinstance(amok._signing_key, SigningKey)
+    assert isinstance(amok._verify_key, VerifyKey)
+
+
+async def test_init_repeat():
+    amok = AmokAPI()
+
+    await amok.init(name="Example")
+
+    signing_key = amok._signing_key
+    verify_key = amok._verify_key
+
+    await amok.init(name="Example")
+
+    assert signing_key == amok._signing_key
+    assert verify_key == amok._verify_key
+
+
+async def test_init_different_names():
+    amok = AmokAPI()
+
+    await amok.init(name="Example")
+
+    signing_key = amok._signing_key
+    verify_key = amok._verify_key
+
+    await amok.init(name="Different")
+
+    assert signing_key != amok._signing_key
+    assert verify_key != amok._verify_key
+
+
+async def test_sign_and_verify():
+    amok = AmokAPI()
+
+    await amok.init(name="Example")
+
+    message = b"Hello, World!"
+    signed_message = await amok.sign(message)
+    signature = signed_message.signature
+    assert isinstance(signature, bytes)
+    assert len(signature) == 64
+
+    assert await amok.verify(amok.verify_key, signed_message.message, signature)
